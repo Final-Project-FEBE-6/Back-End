b340fef42bcf09b719f9f8a7b718582e5a5d154e
VanyaMayazura <90196679+vnymyz@users.noreply.github.com>
2022-11-29 20:41:56 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const DB_URL = 'mongodb://localhost:27017/final-project-BE6';
+const DB_URL = 'mongodb+srv://febe6:febe6123@cluster0.qvutbga.mongodb.net/sapasikolog';
 
 const db = mongoose.connect(DB_URL);
 
