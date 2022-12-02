0362b3fd39d36782b21e5c92ba03c95f0ca09233
arifsetiawan23 <114279571+arifsetiawan23@users.noreply.github.com>
2022-12-02 10:03:38 +0700

    Update artikel.js

--- models/artikel.js
+++ models/artikel.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const artikelSchema = new Schema({
-	judul: String,
+    judul: String,
     isi: String,
     image: String,
     tgl_publish: String,
     sumber: String,
+    nama_sumber: String,
 });
 
 const artikel = mongoose.model('Artikel', artikelSchema);
